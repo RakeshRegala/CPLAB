214d04bd2ce0a5532d642e7cf35cc34b1a8369af
RakeshRegala <150713451+RakeshRegala@users.noreply.github.com>
2024-04-13 17:48:17 +0530

    Update 1.ds.linear search.c

--- 1.ds.linear search.c
+++ 1.ds.linear search.c
@@ -1,6 +1,6 @@
 #include<stdio.h>
 int key;
-int linsearch(int[ ] ,int);
+int linearsearch(int[ ] ,int);
 int main() {
     int i,a[30],n,k;
     printf("Enter no. of elements: ");
@@ -10,14 +10,14 @@ int main() {
         scanf("%d",&a[i]);
     printf("Enter the Key: ");
     scanf("%d",&key);
-    k=linsearch(a,n);
+    k=linearsearch(a,n);
     if(k==-1)
         printf("Key is not Found.");
     else
         printf("Key is found at %d index.",k);
 }
 
-int linsearch(int* p, int n) {
+int linearsearch(int* p, int n) {
     int i;
     for(i=0;i<n;i++)
         if(key== *(p+i))
