5442ec89cecaa9e8bac63c976e32a2a0bba3af18
RakeshRegala <150713451+RakeshRegala@users.noreply.github.com>
2024-04-13 13:12:40 +0530

    Create binarysearch.c

---
+++ binarysearch.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+int key;
+int binsearch(int [ ], int);
+int main() {
+    int i,a[30],n,k;
+    printf("Enter no. of elements: ");
+    scanf("%d",&n);
+    printf("Enter the elements:\n");
+    for(i=0; i<n; i++)
+        scanf("%d",&a[i]);
+    printf("Enter the Key: ");
+    scanf("%d",&key);
+    k=binsearch(a,n);
+    if(k==-1)
+        printf("Key is not found.");
+    else
+        printf("Key is found at %d index.",k);
+}
+
+int binsearch(int* p, int n) {
+    int low,high,mid;
+    low=0;
+    high=n-1;
+    while(low<=high)
+    {
+        mid=(low+high)/2;
+        if(key==*(p+mid))
+            return mid;
+        else if(key<*(p+mid))
+            high=mid-1;
+        else
+            low=mid+1;
+    }
+    return -1;
+}
