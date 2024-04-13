22185b60641391e4c95f1b0ae9f2101172f559a7
RakeshRegala <150713451+RakeshRegala@users.noreply.github.com>
2024-04-13 17:46:27 +0530

    Update 1.ds.insertion.c

--- 1.ds.insertion.c
+++ 1.ds.insertion.c
@@ -1,5 +1,5 @@
 #include<stdio.h>
-void inssort(int [ ], int);
+void insertionsort(int [ ], int);
 int main() {
     int i,a[30],n;
     printf("Enter no. of elements: ");
@@ -7,13 +7,13 @@ int main() {
     printf("Enter the elements:\n");
     for(i=0; i<n; i++)
         scanf("%d",&a[i]);
-    inssort(a,n);
+    insertionsort(a,n);
     printf("After sorting:\n");
     for(i=0; i<n; i++)
         printf("%d ",a[i]);
 }
 
-void inssort(int p[ ], int n) {
+void insertionsort(int p[ ], int n) {
     int i, j, item;
     for(i=1; i<n; i++)
     {
