c4f0bbff21e3a77fa14e867aee2638bc6e0d65f8
RakeshRegala <150713451+RakeshRegala@users.noreply.github.com>
2024-07-22 15:45:14 +0530

    checking the input alphabet is vowel or consonant

---
+++ vowel_consonant.py
@@ -0,0 +1,9 @@
+#FINDING WEATHER AN ALPHABET IS VOWEL OR CONSONANT
+
+
+vowel = ['a','e','i','o','u','A','E','O','I','U']
+alphabet=input("ENTER AN ALPHABET: ")
+if alphabet in vowel:
+ 	print(alphabet,"is a vowel")
+else:
+ 	print(alphabet,"is a consonant")
