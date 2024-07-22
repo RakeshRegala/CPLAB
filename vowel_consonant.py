1de0f4099349a188097bdd040a1b26f2c2a0b76c
RakeshRegala <150713451+RakeshRegala@users.noreply.github.com>
2024-07-22 15:45:50 +0530

    Delete vowel_consonant.py

--- vowel_consonant.py
+++
@@ -1,9 +0,0 @@
-#FINDING WEATHER AN ALPHABET IS VOWEL OR CONSONANT
-
-
-vowel = ['a','e','i','o','u','A','E','O','I','U']
-alphabet=input("ENTER AN ALPHABET: ")
-if alphabet in vowel:
- 	print(alphabet,"is a vowel")
-else:
- 	print(alphabet,"is a consonant")
